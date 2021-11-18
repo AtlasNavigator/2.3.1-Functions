7431ae89c1f43bf2ad0ebdf631f7a1d278291f5d
sgtpeppers <92605951+AtlasNavigator@users.noreply.github.com>
2021-11-18 21:16:11 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// // Functions
+
+// // define
+// function sayHello(){
+//   console.log('Hello');
+// }
+
+// // Call the function
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
