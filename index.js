26593d299e01e30e9108d2006e2f3fd1efeb3e06
Jose  Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2021-11-18 21:32:44 +0000

    More functions examples

--- index.js
+++ index.js
@@ -11,3 +11,40 @@
 // sayHello();
 // sayHello();
 // sayHello();
+
+// // New function
+// function printMax(){
+//  let x = 7;
+//  let y = 10;
+// if (x > 7){
+//   console.log(`The maxiumum numbewr is ${x}`);
+// } else {
+//   console.log(`The maximum number is ${y}`);
+
+// }
+// }
+
+// // call the function
+// printMax();
+
+// // Another one!
+
+// function printMax(x, y){
+// if (x > y){
+//   console.log(`The maxiumum numbewr is ${x}`);
+// } else {
+//   console.log(`The maximum number is ${y}`);
+//   }
+// }
+
+// printMax(5, 7);
+// printMax(100, 12);
+
+// Define the function
+function sum(num1, num2){
+  console.log(num1 + num2);
+}
+
+// Call 'sum'
+sum(5, 6);
+sum('buzz', 'fizz');
